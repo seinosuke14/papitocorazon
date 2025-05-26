4ad9b9fbb288dbede7752e1718510818433d1ddf
seinosuke14 <diegobascur9@gmail.com>
2025-05-26 17:38:07 -0400

    intento de crear app

---
+++ src/app/api/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tuempresa.app',
+  appName: 'NombreApp',
+  webDir: 'npm run build'
+};
+
+export default config;
